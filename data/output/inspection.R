2efc096030c85ecc4987fd2ef520cc73670419f5
gjcooper <gjcooper@gmail.com>
2020-02-11 18:30:21 +1100

    New inspection code for sampled chain

--- data/output/inspection.R
+++ data/output/inspection.R
@@ -19,6 +19,7 @@ burned2 <- burned
 load('cceburn5kfullN3.RData')
 burned3 <- burned
 
+load('ccesample.RData')
 
 alphas <- parameters[grepl("^a.*", parameters)]
 base <- c("A", "b_pos", "b_neg", "t0")
@@ -35,85 +36,34 @@ fourcell <- c("v_pos_HH", "v_neg_HH", "v_pos_HL", "v_neg_HL", "v_pos_LH", "v_neg
 
 #Look at phases etc
 #knitr::kable(table(adaptmk1$samples$stage))
-knitr::kable(table(moreadapted$samples$stage))
+knitr::kable(table(plot_obj$samples$stage))
 
-plot_obj <- moreadapted
-
-# # Alphas
-plot(burned1, pars = alphas)
-dev.new()
-plot(burned2, pars = alphas)
-dev.new()
-plot(burned3, pars = alphas)
-# 
-
-dev.new()
-plot(burned1, pars = base)
-dev.new()
-plot(burned2, pars = base)
-dev.new()
-plot(burned3, pars = base)
-
-dev.new()
-plot(burned1, pars = fourcell)
-dev.new()
-plot(burned2, pars = fourcell)
-dev.new()
-plot(burned3, pars = fourcell)
-
-dev.new()
-plot(burned1, type='alpha', pars=alphas)
-dev.new()
-plot(burned2, type='alpha', pars=alphas)
-dev.new()
-plot(burned3, type='alpha', pars=alphas)
-
-# plot(burned1, type='alpha', pars=base)
-# dev.new()
-# plot(burned2, type='alpha', pars=base)
-# dev.new()
-# plot(burned3, type='alpha', pars=base)
-
-# alphas
-# alphas <- parameters[alphas]
-# dev.new(0
-# dev.new()
-# plot(burned3, pars=alphas)
-# plot(burned2, pars=parameters[posdrift])
-# dev.prev()
-# plot(burned3, pars=parameters[posdrift])
-# dev.next()
-# dev.new()
-# plot(burned2, pars=parameters[posdrift])
-# ls()
-# alldrift
+plot_obj <- sample
 
 plot(plot_obj, pars=alphas)
-plot(plot_obj, pars=base)
-plot(plot_obj, pars=fourcell)
+plot(plot_obj, type='alpha', pars=alphas)
+plot(plot_obj, pars = base)
+plot(plot_obj, type='alpha', pars=base)
+plot(plot_obj, pars = fourcell)
+plot(plot_obj, type='alpha', pars=fourcell)
 plot(plot_obj, pars=posdrift)
+plot(plot_obj, type='alpha', pars=posdrift)
 plot(plot_obj, pars=negdrift)
+plot(plot_obj, type='alpha', pars=negdrift)
 
 mcobj <- mcmc(t(plot_obj$samples$theta_mu))
 
-hd <- heidel.diag(mcobj)
+sobj <- mcmc(t(exp(
+  plot_obj$samples$theta_mu[,plot_obj$samples$stage == 'sample']
+  )))
 
-b1theta_mu <- mcmc(t(burned1$samples$theta_mu))
-b2theta_mu <- mcmc(t(burned2$samples$theta_mu))
-b3theta_mu <- mcmc(t(burned3$samples$theta_mu))
+dimnames(sobj) <- list(NULL,parameters)
 
-colnames(b1theta_mu) <- parameters
-colnames(b2theta_mu) <- parameters
-colnames(b3theta_mu) <- parameters
-plot(b1theta_mu)
-summary(b1theta_mu)
-head(b1theta_mu)
-head(b1theta_mu$alpha_IST)
-dim(b1theta_mu)
-str(b1theta_mu)
-summary(b1theta_mu)$statistics
-rbind(summary(b1theta_mu)$statistics[,'Mean'], summary(b2theta_mu)$statistics[,'Mean'], summary(b3theta_mu)$statistics[,'Mean'])
-means <- exp(rbind(summary(b1theta_mu)$statistics[,'Mean'], summary(b2theta_mu)$statistics[,'Mean'], summary(b3theta_mu)$statistics[,'Mean']))
+hd <- heidel.diag(sobj)
+plot(sobj)
+summary(sobj)$statistics
+
+
 
 means$run <- 1:nrow(means)
 me2 <- pivot_longer(means, -run)
