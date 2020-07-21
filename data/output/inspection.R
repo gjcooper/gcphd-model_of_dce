c75cd2a208de9927aa3773a79282a0aa9772f6d8
gjcooper <gjcooper@gmail.com>
2020-07-21 11:12:01 +1000

    Inspection for all reboot samples

--- data/output/inspection.R
+++ data/output/inspection.R
@@ -1,25 +1,10 @@
 library(coda)
 library(dplyr)
 library(tidyr)
-# load("cceburn5kfull.RData")
-# burned1 <- burned
-# load("cceburn5kfull-2.RData")
-# burned2 <- burned
-# load("cceburn5kfull-3.RData")
-# burned3 <- burned
-# load("cceadapt.RData")
-source("~/PMwG/demo/plot.R")
+require(tcltk)
 
-load('cceadaptmore.RData')
-
-load('cceburn5kfullN1.RData')
-burned1 <- burned
-load('cceburn5kfullN2.RData')
-burned2 <- burned
-load('cceburn5kfullN3.RData')
-burned3 <- burned
-
-load('ccesample.RData')
+datafile <- as.character(tkgetOpenFile())
+load(datafile)
 
 alphas <- parameters[grepl("^a.*", parameters)]
 base <- c("A", "b_pos", "b_neg", "t0")
@@ -36,54 +21,39 @@ fourcell <- c("v_pos_HH", "v_neg_HH", "v_pos_HL", "v_neg_HL", "v_pos_LH", "v_neg
 
 #Look at phases etc
 #knitr::kable(table(adaptmk1$samples$stage))
+plot_obj <- sampled
 knitr::kable(table(plot_obj$samples$stage))
 
-plot_obj <- sample
-
-plot(plot_obj, pars=alphas)
-plot(plot_obj, type='alpha', pars=alphas)
-plot(plot_obj, pars = base)
-plot(plot_obj, type='alpha', pars=base)
-plot(plot_obj, pars = fourcell)
-plot(plot_obj, type='alpha', pars=fourcell)
-plot(plot_obj, pars=posdrift)
-plot(plot_obj, type='alpha', pars=posdrift)
-plot(plot_obj, pars=negdrift)
-plot(plot_obj, type='alpha', pars=negdrift)
-
 mcobj <- mcmc(t(plot_obj$samples$theta_mu))
-
-sobj <- mcmc(t(exp(
-  plot_obj$samples$theta_mu[,plot_obj$samples$stage == 'sample']
-  )))
-
-dimnames(sobj) <- list(NULL,parameters)
-
-hd <- heidel.diag(sobj)
-plot(sobj)
-summary(sobj)$statistics
-
-plot(sobj[,c('alpha_IST', 'alpha_IEX', 'A', 'b_pos', 'b_neg')])
+dimnames(mcobj) <- list(NULL,parameters)
+plot(mcobj, smooth=TRUE)
 
 
-means$run <- 1:nrow(means)
-me2 <- pivot_longer(means, -run)
-ggplot(me2) + geom_point()
-ggplot(me2) + geom_point(aes(x=name, y=value))
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run))
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_discrete()
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_continuous()
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_brewer()
-table(me2$run)
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_manual()
-me2$run <- as.factor(me2$run)
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run))
-me2
-me2[me2$name != alpha_IST]
-me2[me2$name != 'alpha_IST']
-me2[me2$name != 'alpha_IST',]
-me3 = me2[me2$name != 'alpha_IST',]
-ggplot(me3) + geom_point(aes(x=name, y=value, colour=run))
-ggplot(me2) + geom_point(aes(x=name, y=value, colour=run))
-history
-history()
+# hd <- heidel.diag(sobj)
+# plot(sobj)
+# summary(sobj)$statistics
+
+# plot(sobj[,c('alpha_IST', 'alpha_IEX', 'A', 'b_pos', 'b_neg')])
+
+
+# means$run <- 1:nrow(means)
+# me2 <- pivot_longer(means, -run)
+# ggplot(me2) + geom_point()
+# ggplot(me2) + geom_point(aes(x=name, y=value))
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run))
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_discrete()
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_continuous()
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_brewer()
+# table(me2$run)
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run)) + scale_colour_manual()
+# me2$run <- as.factor(me2$run)
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run))
+# me2
+# me2[me2$name != alpha_IST]
+# me2[me2$name != 'alpha_IST']
+# me2[me2$name != 'alpha_IST',]
+# me3 = me2[me2$name != 'alpha_IST',]
+# ggplot(me3) + geom_point(aes(x=name, y=value, colour=run))
+# ggplot(me2) + geom_point(aes(x=name, y=value, colour=run))
+# history
+# history()
