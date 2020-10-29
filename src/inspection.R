7cb439ef60ab2299ceca4996c2140c6a6f77c4d5
gjcooper <gjcooper@gmail.com>
2020-10-30 10:54:14 +1100

    Refactor for repetition, add comparison code

--- src/inspection.R
+++ src/inspection.R
@@ -25,19 +25,13 @@ plot_theta_mu <- function(plot_obj) {
   plot(samples, smooth=TRUE)
 }
 
-#' Plot the evidence for each model by subject
-#'
-#' @param plot_obj - the pmwg sampler object containing estimates
-#' @param relative - Whether to plot the evidence as relative or absolute
-#'
-#' @return None - side effect is the creation of the plot
-look_at_alphas <- function(plot_obj, relative=TRUE) {
+collect_samples <- function(plot_obj) {
   tmus <- as_mcmc(plot_obj, filter="sample")
   dimnames(tmus) <- list(NULL, parameters)
   tmu_alphas <- tmus[, 1:7] %>% data.frame() %>% tibble()
   tmu_alphas$subjectid <- 'theta_mu'
 
-  res <- as_mcmc(plot_obj, selection="alpha", filter="sample") %>%
+  as_mcmc(plot_obj, selection="alpha", filter="sample") %>%
     lapply(FUN = function(X) {
       dimnames(X) <- list(NULL, parameters)
       data.frame(X[, 1:7])
@@ -45,9 +39,18 @@ look_at_alphas <- function(plot_obj, relative=TRUE) {
   bind_rows(.id="subjectid") %>%
   tibble() %>%
   rbind(tmu_alphas)
+}
 
-
-  medians <- res %>% group_by(subjectid) %>% summarise(across(.fns=median)) %>%
+#' Plot the evidence for each model by subject
+#'
+#' @param plot_obj - the pmwg sampler object containing estimates
+#' @param relative - Whether to plot the evidence as relative or absolute
+#'
+#' @return None - side effect is the creation of the plot
+look_at_alphas <- function(plot_obj, relative=TRUE) {
+  medians <- collect(plot_obj) %>%
+    group_by(subjectid) %>%
+    summarise(across(.fns=median)) %>%
     mutate_at(vars(contains("alpha")), exp) %>%
     pivot_longer(-subjectid) %>%
     rename(Parameter=name) %>%
@@ -73,7 +76,52 @@ look_at_alphas <- function(plot_obj, relative=TRUE) {
   }
 }
 
+
+get_medians <- function(res) {
+  res %>%
+    group_by(subjectid) %>%
+    summarise(across(.fns = median)) %>%
+    mutate_at(vars(contains("alpha")), exp) %>%
+    pivot_longer(-subjectid) %>%
+    rename(Parameter = name) %>%
+    group_by(subjectid) %>%
+    mutate(allvals = sum(value)) %>%
+    mutate(relative_evidence = value / allvals) %>%
+    mutate(Parameter = as.factor(Parameter))
+}
+
+compare <- function(original, recovery) {
+  original <- collect_samples(original)
+  recovery <- collect_samples(recovery)
+
+
+  omedians <- get_medians(original)
+  rmedians <- get_medians(recovery)
+  par_labels = sapply(strsplit(levels(omedians$Parameter), "_"), function(x) {x[2]})
+
+  medians <- bind_rows(estimates = omedians, recovery = rmedians, .id = "source")
+
+  if (relative) {
+    ggplot(medians, aes(x=subjectid, y=relative_evidence, fill=Parameter)) +
+      geom_col() +
+      xlab("Subject Identifier") +
+      ylab("Relative Evidence") +
+      scale_fill_brewer(palette="Set2", name = "Model", labels = par_labels) +
+      facet_grid(rows = vars(source))
+  } else {
+    ggplot(medians, aes(x=subjectid, y=value, fill=Parameter)) +
+      geom_col() +
+      xlab("Subject Identifier") +
+      ylab("Absolute Evidence") +
+      scale_fill_brewer(palette="Set2", name = "Model", labels = par_labels) +
+      facet_grid(rows = vars(source))
+  }
+}
+
+
 #Look at the estimates in absolute and relative terms, then use coda mcmc plot for theta_mu
+recovery <- sampled
+original <- sampled
 look_at_alphas(sampled)
 look_at_alphas(sampled, relative=FALSE)
 plot_theta_mu(sampled)
