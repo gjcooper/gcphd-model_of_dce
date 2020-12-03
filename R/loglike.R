54cc97010ca9191762a103a653398b1483f01a90
gjcooper <gjcooper@gmail.com>
2020-12-03 21:54:02 +1100

    Initial move towards package structure

--- R/loglike.R
+++ R/loglike.R
@@ -1,9 +1,3 @@
-library(MCMCpack)
-
-# Simulation for sampling
-
-# Specify likelihoods of parameters for individual models ---------------------
-
 # Independant parallel self terminating
 ll_IST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
@@ -149,6 +143,37 @@ ll_funcs <- c(ll_IST, ll_IEX, ll_CYST, ll_CYEX, ll_CNST, ll_CNEX, ll_CB)
 rll_funcs <- c(rll_IST, rll_IEX, rll_CYST, rll_CYEX, rll_CNST, rll_CNEX, rll_CB)
 ll_names <- c("IST", "IEX", "CYST", "CYEX", "CNST", "CNEX", "CB")
 
+#' Top level log-likelihood function that implements the dirichlet sampling
+#'
+#' This function selects one of the possible architectures using a random
+#' dirichlet sample weighted by the α values from the parameter vector. It
+#' then delegates the likelihood for the data to an architecture specific
+#' log-likelihood function.
+#'
+#' @section The parameter vector:
+#'
+#' The vector x should contain the following elements:
+#' A number of \eqn{\alpha}
+#' values 
+#' 
+#' \itemize{
+#'   \item A number of \strong{\eqn{\alpha}} parameter values matching the number and
+#'     order of the ll_funcs vector defined in this same file.
+#'   \item \strong{A} - the start point variability
+#'   \item \strong{b^a} and \strong{b^r}, the thresholds to either accept or
+#'     reject the item.
+#'   \item \strong{t0} - the residual time, bounded above by the minimum
+#'     response time for the participant
+#'   \item 12 drift rates. For each attribute there are three stimulus levels.
+#'     For each of these 6 attribute levels there are two drift rates, one drift
+#'     rate to accept (\strong{v^a}) and one to reject (\strong{v^r})
+#' }
+#'
+#' @param x A named vector containing parameter values to test
+#' @param data The data for a single subject for which the likelihood should be
+#'   calculated
+#'
+#' @return The log of the lilelihood for the data under paramweter values x
 model_wrapper <- function(x, data, model) {
   ydat <- data[data$response == 2, ]
   ndat <- data[data$response == 1, ]
@@ -168,21 +193,22 @@ model_wrapper <- function(x, data, model) {
   c(yes, no)
 }
 
+
 #' Top level log-likelihood function that implements the dirichlet sampling
 #'
 #' This function selects one of the possible architectures using a random
-#' dirichlet sample weighted by the \alpha values from the parameter vector. It
+#' dirichlet sample weighted by the α values from the parameter vector. It
 #' then delegates the likelihood for the data to an architecture specific
 #' log-likelihood function.
 #'
 #' @section The parameter vector:
 #'
 #' The vector x should contain the following elements:
-#' A number of /alpha
+#' A number of \eqn{\alpha}
 #' values 
 #' 
 #' \itemize{
-#'   \item A number of \strong{\alpha} parameter values matching the number and
+#'   \item A number of \strong{\eqn{\alpha}} parameter values matching the number and
 #'     order of the ll_funcs vector defined in this same file.
 #'   \item \strong{A} - the start point variability
 #'   \item \strong{b^a} and \strong{b^r}, the thresholds to either accept or
@@ -212,39 +238,11 @@ dirichlet_mix_ll <- function(x, data) {
   x["b_neg"] <- x["b_neg"] + x["A"]
 
   # all decision rules
-  rdev <- rdirichlet(1, x[mix_counts])
+  rdev <- MCMCpack::rdirichlet(1, x[mix_counts])
   func_idx <- sample(mix_counts, 1, prob = rdev)
   ll_func <- ll_funcs[[func_idx]]
   trial_ll <- model_wrapper(x, data, ll_func)
   new_like <- (1 - p_contam) * trial_ll +
-    p_contam * (dunif(data$rt, min_rt, max_rt) / 2)
+    p_contam * (stats::dunif(data$rt, min_rt, max_rt) / 2)
   sum(log(pmax(new_like, 1e-10)))
-}
-
-test.likelihood <- function(x, num.values = 9, fake.data, dimensions, ll_func, server = FALSE, ...) {
-  pars <- names(x)
-  x.tmp <- x
-  sequence <- seq(from = -.22, to = .22, length.out = num.values)
-  op <- par(mfrow = dimensions)
-  par_likelihoods <- lapply(setNames(pars, pars), function(p) {
-    testvalues <- sequence + true.x[[p]]
-    tmp <- unlist(lapply(testvalues, function(i) { # for each test vlaue, it will apply the function where i = testvalue.
-      x.tmp[[p]] <- i
-      ll_func(x = x.tmp, data = fake.data)
-    }))
-
-    plot(
-      x = testvalues,
-      y = tmp,
-      type = "b",
-      main = p,
-      xlab = "log par values",
-      ylab = "loglikelihood",
-      ...
-    )
-    abline(v = true.x[[p]], col = "red")
-    return(tmp)
-  })
-  par(op)
-  return(par_likelihoods)
 }
---
+++ R/mcce-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+# The following block is used by usethis to automatically manage
+# roxygen namespace tags. Modify with care!
+## usethis namespace: start
+## usethis namespace: end
+NULL
+
+#' @importFrom rtdists plba_norm dlba_norm rlba_norm
+NULL
