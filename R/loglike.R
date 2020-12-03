4a75abf9f653f3633b2a69234aad2d7e18503a6b
gjcooper <gjcooper@gmail.com>
2020-12-03 22:06:49 +1100

    Add docs to all loglike funcs, moved scripts to src

--- R/loglike.R
+++ R/loglike.R
@@ -1,4 +1,16 @@
-# Independant parallel self terminating
+#' Independant Self Terminating
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_IST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- 2 *
@@ -13,7 +25,6 @@ ll_IST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_IST <- function(x, data) {
   data$response <- NA
   data$rt <- NA
@@ -37,6 +48,20 @@ rll_IST <- function(x, data) {
   data
 }
 
+
+#' Independant Exhaustive model
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_IEX <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- 2 *
@@ -51,11 +76,24 @@ ll_IEX <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_IEX <- function(x, data) {
   stop("Not implemented yet")
 }
 
+
+#' Coactive Yes Self terminating No
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_CYST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- dlba_norm(rt, 2 * A, 2 * b_pos, t0, 2 * v_pos, sqrt(2)) * # nolint
@@ -68,11 +106,24 @@ ll_CYST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_CYST <- function(x, data) {
   stop("Not implemented yet")
 }
 
+
+#' Coactive Yes Exhaustive No
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_CYEX <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- dlba_norm(rt, 2 * A, 2 * b_pos, t0, 2 * v_pos, sqrt(2)) * # nolint
@@ -85,11 +136,24 @@ ll_CYEX <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_CYEX <- function(x, data) {
   stop("Not implemented yet")
 }
 
+
+#' Coactive No/Self terminating Yes
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_CNST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- 2 *
@@ -102,11 +166,24 @@ ll_CNST <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_CNST <- function(x, data) {
   stop("Not implemented yet")
 }
 
+
+#' Coactive No / Exhaustive Yes
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_CNEX <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- 2 *
@@ -119,11 +196,24 @@ ll_CNEX <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_CNEX <- function(x, data) {
   stop("Not implemented yet")
 }
 
+
+#' Coactive Both
+#'
+#' @param rt A vector of response times
+#' @param A Start point variability 
+#' @param b_pos positive evidence threshold
+#' @param b_neg negative evidence threshold
+#' @param t0 non decision time parameter
+#' @param v_pos positive evidence drift rate (vector of)
+#' @param v_neg vector of negative evidence drift rates
+#' @param positive Whether we are looking at accept or reject trials
+#'
+#' @return The log likelihood of the rts for the accept or reject trials given
+#'   the provided parameter values
 ll_CB <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   if (positive) {
     ll <- dlba_norm(rt, 2 * A, 2 * b_pos, t0, 2 * v_pos, sqrt(2)) * # nolint
@@ -134,7 +224,6 @@ ll_CB <- function(rt, A, b_pos, b_neg, t0, v_pos, v_neg, positive) { # nolint
   }
   ll
 }
-
 rll_CB <- function(x, data) {
   stop("Not implemented yet")
 }
@@ -143,37 +232,18 @@ ll_funcs <- c(ll_IST, ll_IEX, ll_CYST, ll_CYEX, ll_CNST, ll_CNEX, ll_CB)
 rll_funcs <- c(rll_IST, rll_IEX, rll_CYST, rll_CYEX, rll_CNST, rll_CNEX, rll_CB)
 ll_names <- c("IST", "IEX", "CYST", "CYEX", "CNST", "CNEX", "CB")
 
-#' Top level log-likelihood function that implements the dirichlet sampling
+
+#' Wrapper for individual model log likelihood function
 #'
-#' This function selects one of the possible architectures using a random
-#' dirichlet sample weighted by the α values from the parameter vector. It
-#' then delegates the likelihood for the data to an architecture specific
-#' log-likelihood function.
+#' This function performs some common steps such as rearrangeing the data
+#' pulling out parameter items into variable and combining the results of
+#' applying to model to different subsets of the data (accept and reject
+#' responses)
 #'
-#' @section The parameter vector:
+#' @inheritParams dirichlet_mix_ll
+#' @param model The model to be wrapped and returned
 #'
-#' The vector x should contain the following elements:
-#' A number of \eqn{\alpha}
-#' values 
-#' 
-#' \itemize{
-#'   \item A number of \strong{\eqn{\alpha}} parameter values matching the number and
-#'     order of the ll_funcs vector defined in this same file.
-#'   \item \strong{A} - the start point variability
-#'   \item \strong{b^a} and \strong{b^r}, the thresholds to either accept or
-#'     reject the item.
-#'   \item \strong{t0} - the residual time, bounded above by the minimum
-#'     response time for the participant
-#'   \item 12 drift rates. For each attribute there are three stimulus levels.
-#'     For each of these 6 attribute levels there are two drift rates, one drift
-#'     rate to accept (\strong{v^a}) and one to reject (\strong{v^r})
-#' }
-#'
-#' @param x A named vector containing parameter values to test
-#' @param data The data for a single subject for which the likelihood should be
-#'   calculated
-#'
-#' @return The log of the lilelihood for the data under paramweter values x
+#' @return The log of the likelihood for the data under parameter values x
 model_wrapper <- function(x, data, model) {
   ydat <- data[data$response == 2, ]
   ndat <- data[data$response == 1, ]
@@ -224,7 +294,7 @@ model_wrapper <- function(x, data, model) {
 #' @param data The data for a single subject for which the likelihood should be
 #'   calculated
 #'
-#' @return The log of the lilelihood for the data under paramweter values x
+#' @return The log of the likelihood for the data under parameter values x
 dirichlet_mix_ll <- function(x, data) {
   x <- exp(x)
 
