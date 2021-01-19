9c50b28c58e807db4536d8e554626a3aea382d25
gjcooper <gjcooper@gmail.com>
2021-01-19 14:26:29 +1100

    New flexible parameter building (from loglike code)

--- src/model_estimation_Exp1.R
+++ src/model_estimation_Exp1.R
@@ -78,10 +78,8 @@ acc_rej_drift <- c("v_acc_p", "v_acc_r", "v_rej_p", "v_rej_r")
 stim_levels <- c("H", "L", "D")
 
 parameters <- c(
-  # Parallel mixture counts
-  "alpha_IST", "alpha_IEX",
-  # Coactive mixture probabilities
-  "alpha_CB",
+  # alpha (dirichlet mixture pars) for each likelihood function exposed in mcce
+  apply(expand.grid("alpha", names(ll_funcs)), 1, paste, collapse = "_"),
   # A - start point variability (sampled from U(0, A) where U is uniform dist)
   "A",
   # b_acc - threshold to accept based on evidence accumulaton in channel
@@ -89,12 +87,11 @@ parameters <- c(
   # b_rej - threshold to reject based on evidence accumulation in channel
   "b_rej",
   # t0 - residual time, bounded above by min response time for participant k
-  "t0"
+  "t0",
+  # Drift rates rto accept/reject for different stimulus levels/attributes
+  apply(expand.grid(acc_rej_drift, stim_levels), 1, paste, collapse = "_")
 )
-parameters <- c(parameters, apply(
-  expand.grid(acc_rej_drift, stim_levels), 1, paste,
-  collapse = "_"
-))
+
 # Mixture counts should always come first
 mix_counts <- 1:sum(startsWith(parameters, "alpha"))
 
