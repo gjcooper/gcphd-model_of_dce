bf1ebedbefd4571b1de8312d64bfd786c3af410e
Gavin Cooper <phd@gavincooper.net>
2021-06-08 15:23:58 +1000

    Trial new functions using abbreviations

---
+++ src/abbrev.R
@@ -0,0 +1,24 @@
+dap <- function() {
+  dlba_norm(rt, A, b_acc, t0, drifts$AccPrice, 1)
+}
+drp <- function() {
+  dlba_norm(rt, A, b_rej, t0, drifts$RejPrice, 1)
+}
+dar <- function() {
+  dlba_norm(rt, A, b_acc, t0, drifts$AccRating, 1)
+}
+drr <- function() {
+  dlba_norm(rt, A, b_rej, t0, drifts$RejRating, 1)
+}
+pap <- function() {
+  plba_norm(rt, A, b_acc, t0, drifts$AccPrice, 1)
+}
+prp <- function() {
+  plba_norm(rt, A, b_rej, t0, drifts$RejPrice, 1) 
+}
+par <- function() {
+  plba_norm(rt, A, b_acc, t0, drifts$AccRating, 1)
+}
+prr <- function() {
+  plba_norm(rt, A, b_rej, t0, drifts$RejRating, 1)
+}
--- src/ll_test.R
+++ src/ll_test.R
@@ -1,72 +1,184 @@
 library(dplyr)
 library(tidyr)
 pkg <- asNamespace("mcce")
+library(mcce)
+library(rtdists)
+source("abbrev.R")
+library(ggplot2)
+library(patchwork)
+library(parallel)
 
+cells <- c("HH", "HL", "LH", "LL", "HD", "LD", "DH", "DL", "DD")
 
-medians <- readRDS(here::here("data", "output", "median_alpha_exp1.RDS"))
-example_vals <- medians[5, ] %>% round(1)
-test_vals <- example_vals %>%
-  dplyr::select(!(starts_with("v") | starts_with("alpha")))
+get_plausible_vals <- function() {
+  medians <- readRDS(here::here("data", "output", "median_alpha_exp1.RDS"))
+  example_vals <- medians %>% summarise_all(mean) %>% round(1)
+  test_vals <- example_vals %>%
+    dplyr::select(!(starts_with("v") | starts_with("alpha"))) %>%
+    mutate(t0 = -Inf)
 
-cell <- "HH"
-drifts <- example_vals %>%
-  dplyr::select(starts_with("v")) %>%
-  pivot_longer(
-    cols = everything(),
-    names_sep = "_",
-    names_to = c("drift", "accept", "attr", "level")
-  ) %>%
-  mutate(
-    attr = ifelse(attr == "p", "Price", "Rating"),
-    accept = ifelse(accept == "acc", "Acc", "Rej")
-  ) %>%
-  mutate(accattr = paste0(accept, attr)) %>%
-  dplyr::select(-drift) %>%
-  filter(if_else(
-    attr == "Price" & level == substring(cell, 1, 1) |
-    attr == "Rating" & level == substring(cell, 2, 2),
-    TRUE,
-    FALSE
-  )) %>%
-  dplyr::select(accattr, value) %>%
-  pivot_wider(names_from=accattr, values_from=value)
-  
-drifts
+  drifts <- sapply(cells, function(cell) {
+    drifts <- example_vals %>%
+      dplyr::select(starts_with("v")) %>%
+      pivot_longer(
+        cols = everything(),
+        names_sep = "_",
+        names_to = c("drift", "accept", "attr", "level")
+      ) %>%
+      mutate(
+        attr = ifelse(attr == "p", "Price", "Rating"),
+        accept = ifelse(accept == "acc", "Acc", "Rej")
+      ) %>%
+      mutate(accattr = paste0(accept, attr)) %>%
+      dplyr::select(-drift) %>%
+      filter(if_else(
+        attr == "Price" & level == substring(cell, 1, 1) |
+        attr == "Rating" & level == substring(cell, 2, 2),
+        TRUE,
+        FALSE
+      )) %>%
+      dplyr::select(accattr, value) %>%
+      pivot_wider(names_from = accattr, values_from = value)
+  }, simplify = FALSE)
+  list(
+    all = example_vals,
+    fixed = test_vals,
+    drifts = drifts
+  )
+}
 
 # Generate samples using rll_func
-n_rsamples <- 1e6 + 8
-n_outer <- n_rsamples / 3
-n_inner <- n_outer / 3
-test_trials <- tibble(
-  price = as.factor(rep(c("H", "L", "D"), each = n_outer)),
-  rating = as.factor(rep(c("H", "L", "D"), each = n_inner, times = 3))
-  ) %>%
+get_predicted_data <- function(trials_per_cell, pars, func) {
+  test_trials <- tibble(
+    price = as.factor(rep(c("H", "L", "D"), each = trials_per_cell * 3)),
+    rating = as.factor(rep(c("H", "L", "D"), each = trials_per_cell, times = 3))
+    ) %>%
+    mutate(
+      v_acc_p = paste0("v_acc_p_", price),
+      v_rej_p = paste0("v_rej_p_", price),
+      v_acc_r = paste0("v_acc_r_", rating),
+      v_rej_r = paste0("v_rej_r_", rating)
+    )
+
+  test_data <- rmodel_wrapper(pars, test_trials, func) %>%
+    mutate(cell = paste0(price, rating)) %>%
+    mutate(cell = factor(cell, levels = cells))
+}
+
+run_integrate <- function(drifts, pars, func) {
+  accept_int <- sapply(drifts, function(drifts_tbl) {
+    integrate_args <- c(
+      as.list(pars),
+      drifts = list(drifts_tbl),
+      f = simple_model_wrapper,
+      lower = 0,
+      upper = 100,
+      accept = TRUE,
+      model = func
+      )
+    int_res <- do.call(integrate, integrate_args)
+    int_res$value
+  })
+  reject_int <- sapply(drifts, function(drifts_tbl) {
+    integrate_args <- c(
+      as.list(pars),
+      drifts = list(drifts_tbl),
+      f = simple_model_wrapper,
+      lower = 0,
+      upper = 100,
+      accept = FALSE,
+      model = func
+      )
+    int_res <- do.call(integrate, integrate_args)
+    int_res$value
+  })
+  bind_cols(accept = accept_int, reject = reject_int, cell = names(accept_int))
+}
+
+get_proportions <- function(pp_data, int_data) {
+  sample_prop <- pp_data %>%
+    count(cell, accept) %>%
+    group_by(cell) %>%
+    mutate(prop = prop.table(n)) %>%
+    filter(accept == 2) %>%
+    dplyr::select(-c(accept, n))
+
+  combined <- int_data %>%
+    inner_join(sample_prop, by = "cell") %>%
+    relocate(c(cell, accept, reject, prop)) %>%
+    mutate(adiff = prop - accept, rdiff = prop - (1 - reject)) %>%
+    mutate(across(where(is.double), round, 2))
+
+  combined
+}
+
+new_IST <- function(rt, A, b_acc, b_rej, t0, drifts, accept) { # nolint
+  if (accept) {
+    ll <- dap() * (1 - par()) * (1 - prp()) * (1 - prr()) +
+          dap() * (1 - par()) * (1 - prp()) * prr() +
+          dap() * (1 - par()) * prp() * (1 - prr()) +
+          dar() * (1 - pap()) * (1 - prp()) * (1 - prr()) +
+          dar() * (1 - pap()) * (1 - prp()) * prr() +
+          dar() * (1 - pap()) * prp() * (1 - prr())
+  } else {
+    ll <- drp() * drr() * (1 - pap()) * (1 - par())
+  }
+  ll
+}
+
+new_IEX <- function(rt, A, b_acc, b_rej, t0, drifts, accept) { # nolint
+  if (accept) {
+    ll <- dap() * dar() * (1 - prp()) * (1 - prr())
+  } else {
+    ll <- drp() * (1 - prr()) * (1 - pap()) * (1 - par()) +
+          drp() * (1 - prr()) * (1 - pap()) * par() +
+          drp() * (1 - prr()) * pap() * (1 - par()) +
+          drr() * (1 - prp()) * (1 - pap()) * (1 - par()) +
+          drr() * (1 - prp()) * (1 - pap()) * par() +
+          drr() * (1 - prp()) * pap() * (1 - par())
+  }
+  ll
+}
+
+function_list <- list(
+  old_IST = list(ll = pkg$ll_IST, rsample = pkg$rll_IST),
+#  new_IST = list(ll = new_IST, rsample = pkg$rll_IST),
+  old_IEX = list(ll = pkg$ll_IEX, rsample = pkg$rll_IEX),
+#  new_IEX = list(ll = new_IEX, rsample = pkg$rll_IEX),
+  old_CB = list(ll = pkg$ll_CB, rsample = pkg$rll_CB),
+  new_FPP = list(ll = pkg$ll_FPP, rsample = pkg$rll_FPP),
+  new_MW = list(ll = pkg$ll_MW, rsample = pkg$rll_MW)
+)
+
+
+# Start the clock!
+ptm <- proc.time()
+all_runs <- lapply(function_list, function(func_pair) {
+  pars <- get_plausible_vals()
+  pred <- get_predicted_data(10, pars$all, func_pair$rsample)
+  intf <- run_integrate(pars$drifts, pars$fixed, func_pair$ll)
+  res <- get_proportions(pred, intf)
+  res
+})
+# Stop the clock
+proc.time() - ptm
+
+combined <- bind_rows(all_runs, .id = "function_set") %>%
   mutate(
-    v_acc_p = paste0("v_acc_p_", price),
-    v_rej_p = paste0("v_rej_p_", price),
-    v_acc_r = paste0("v_acc_r_", rating),
-    v_rej_r = paste0("v_rej_r_", rating)
+    cell = factor(cell, levels = cells),
+    function_set = factor(function_set, levels=names(function_list))
   )
+acc_diff <- combined %>%
+  ggplot(aes(x = cell, y = adiff, colour = function_set, group = function_set)) +
+  geom_line() +
+  labs(title = "% Accept in samples - integrate using accept = TRUE",
+               x = "Cell from Design", y = "Difference") +
+  ylim(-1, 1)
+rej_diff <- combined %>%
+  ggplot(aes(x = cell, y = rdiff, colour = function_set, group = function_set)) +
+  geom_line() +
+  labs(title = "% Accept in samples - (1 - integrate using accept = FALSE)",
+               x = "Cell from Design", y = "Difference") +
+  ylim(-1, 1)
 
-test_data <- rmodel_wrapper(test_vals, test_trials, pkg$rll_IST)
-
-integrate_args <- c(
-  as.list(test_vals),
-  drifts = list(drifts),
-  f = simple_model_ll,
-  lower = unname(unlist(exp(test_vals["t0"]))) + 1e-3,
-  upper = Inf,
-  accept = TRUE,
-  model = pkg$ll_IST
-  )
-
-inttest <- c(
-  as.list(test_vals),
-  drifts = list(drifts)
-  )
-
-do.call(integrate, integrate_args)
-
-integrate(f, lower, upper, ..., subdivisions = 100L,
-          rel.tol = .Machine$double.eps^0.25, abs.tol = rel.tol,
-          stop.on.error = TRUE, keep.xy = FALSE, aux = NULL)
+acc_diff / rej_diff
