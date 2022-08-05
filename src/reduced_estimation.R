b3eb0bbf4c8766b9976b30e3b00bddb1870ab2dc
Gavin Cooper <phd@gavincooper.net>
2022-08-05 14:11:57 +1000

    Trial staged reduction in burn epsilon

--- src/reduced_estimation.R
+++ src/reduced_estimation.R
@@ -110,15 +110,10 @@ sampler <- pmwgs(
 
 sampler <- init(sampler)
 
-sampler <- run_stage(sampler, stage = "burn", iter = 50, particles = 500, epsilon = 1, n_cores = cores)
-sampler <- run_stage(sampler, stage = "burn", iter = 5000, particles = 500, n_cores = cores)
-
-save.image(outfile)
-
-sampler <- run_stage(sampler, stage = "adapt", iter = 10000, particles = 500, n_cores = cores, n_unique = 40)
-
-save.image(outfile)
-
-sampler <- run_stage(sampler, stage = "sample", iter = 10000, particles = 100, n_cores = cores, pdist_update_n = NA)
+sampler <- run_stage(sampler, stage = "burn", iter = 1000, particles = 500, epsilon = 1, n_cores = cores)
+sampler <- run_stage(sampler, stage = "burn", iter = 1000, particles = 500, epsilon = 0.9, n_cores = cores)
+sampler <- run_stage(sampler, stage = "burn", iter = 1000, particles = 500, epsilon = 0.8, n_cores = cores)
+sampler <- run_stage(sampler, stage = "burn", iter = 1000, particles = 500, epsilon = 0.7, n_cores = cores)
+sampler <- run_stage(sampler, stage = "burn", iter = 1000, particles = 500, epsilon = 0.6, n_cores = cores)
 
 save.image(outfile)
