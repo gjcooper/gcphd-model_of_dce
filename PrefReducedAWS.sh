1baa0f368b4d9eff826f01cfe8f6d403722a61bc
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2022-08-11 00:04:41 +0000

    Clean AWS scripts

--- PrefReducedAWS.sh
+++ PrefReducedAWS.sh
@@ -5,22 +5,10 @@ export DCE_EXP_DATA="Pref_preprocessed.RDS"
 export DCE_MIN_RT=0.35
 export DCE_MAX_RT=10
 export DCE_CONTAM=0.02
-export VDCE_TAG="reduced"
+export VDCE_TAG="staged_burn"
 
 # Required for gsl (used by rtdists), due to our installation method.
 export LD_LIBRARY_PATH="/usr/local/gsl/2.5/x86_64/lib64"
-
-function notify() {
-  message="${1}"
-  instance_identity="$(curl http://169.254.169.254/latest/dynamic/instance-identity/document)"
-  account_id="$(echo "${instance_identity}" | jq -r '.accountId')"
-  region="$(echo "${instance_identity}" | jq -r '.region')"
-  sns_topic_name="uonswpoc-alerts"  # TODO: Retrieve this name from somewhere, potentially CFN exports or tags?
-  topic_arn="arn:aws:sns:${region}:${account_id}:${sns_topic_name}"
-
-  echo "${message}" && echo
-  aws sns publish --message "${message}" --region "${region}" --topic-arn "${topic_arn}"
-}
 
 Rscript --no-save --no-restore src/reduced_estimation.R > estimation_$DCE_EST_EXP.$VDCE_TAG.out 2> estimation_$DCE_EST_EXP.$VDCE_TAG.err
 
